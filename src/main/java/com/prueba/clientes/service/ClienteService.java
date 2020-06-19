0e00d698509aad6eb133745f305f2e640ef15d0d
Paul Santibanez <paul.santibanezdev@gmail.com>
2020-06-19 02:11:05 +0200

    version 2

--- src/main/java/com/prueba/clientes/service/ClienteService.java
+++ src/main/java/com/prueba/clientes/service/ClienteService.java
@@ -1,12 +1,18 @@
 package com.prueba.clientes.service;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.prueba.clientes.model.Cliente;
 import com.prueba.clientes.repository.ClienteRepository;
-import lombok.Getter;
-import lombok.Setter;
+import java.util.concurrent.ThreadLocalRandom;
 @Service
 public class ClienteService {
 
@@ -14,7 +20,28 @@ public class ClienteService {
 	private ClienteRepository clientrepo;
 	
 	public Cliente createClient(Cliente cliente) throws Exception {
-		System.out.print(cliente.getApellido());
+		  Calendar cal = Calendar.getInstance();
+ 		int randomNum = ThreadLocalRandom.current().nextInt(71, 78 + 1);
+ 		Date nac = cliente.getFec_nac();
+		  cal.setTime(nac);
+		  cal.add(Calendar.YEAR,  randomNum  ); 
+		  Date muerte = cal.getTime();
+		  String muernew = new SimpleDateFormat("yyyy-MM-dd").format(muerte);
+		  cliente.setFec_muerte(muernew);
 		return clientrepo.save(cliente);
 	}
+	
+	  public Map<String, Object> kpiCliente(){
+	      Map<String, Object> map = new HashMap<String, Object>();
+	      float promedio= clientrepo.promedio();
+	      float desviacion=  clientrepo.desviacion();	  
+	      map.put("promedio", promedio);
+	      map.put("desviacion", desviacion);
+	      return map;      
+	   }
+	  
+	  public List<Cliente> listClient() throws Exception {
+		 return  clientrepo.findAll();
+		
+		}
 }
